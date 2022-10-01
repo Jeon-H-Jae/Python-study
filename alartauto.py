f00f2bc0a895f980d733e00cf6aa2c5861396444
Jeon-H-Jae <heejamm2354@gmail.com>
2022-10-01 11:35:10 +0900

    .

---
+++ alartauto.py
@@ -0,0 +1,6 @@
+import pyautogui
+# 알람제어 pyautogui.alert(text,title,button_string)
+# Yes or No 알림창 함수 pyautogui.confirm()
+# 텍스트입력 pyautogui.prompt(text,titile)
+# 비밀번호 pyautogui.password(text,titile,mask)
+pyautogui.imgauto()
---
+++ keyauto.py
@@ -0,0 +1,5 @@
+import pyautogui
+# 키보로 문장을 작성하는 함수 pyautogui.write()
+# 키입력 함수 pyautogui.press()
+# 키보드 원하는 키 누르고 때기 pyautogui.keyDown() , pyautogui.keyUp()
+# 핫키제어 pyautogui.hotkey() ex) 복사하기 pyautogui.hotkey("Ctrl","c") 
